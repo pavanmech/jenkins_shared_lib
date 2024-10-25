8568d30898c5b8e98c9d36c7cf3f87222610c3d0
pavanmech <153759227+pavanmech@users.noreply.github.com>
2024-10-25 22:36:45 +0530

    Update pushEcrRepo2.groovy

--- vars/pushEcrRepo2.groovy
+++ vars/pushEcrRepo2.groovy
@@ -1,1 +1,46 @@
+adef call(Map pipelineParams){
+def projectName = "${pipelineParams.ecrRepoName}"
 
+pipeline {
+   agent any
+   environment
+    {
+        VERSION = "${BUILD_NUMBER}"
+        PROJECT = "${projectName}"
+        IMAGE = "$PROJECT:$VERSION"
+        ECRURL = 'https://533267166046.dkr.ecr.ap-south-1.amazonaws.com/${projectName}'
+        ECRCRED = 'ecr:ap-south-1:aws_credentials'
+    }
+    stages {
+         stage('Image Build'){
+             steps{
+                 script{
+                       docker.build('$IMAGE')
+                 }
+             }
+         }
+         stage('Push Image'){
+         steps{
+             script
+                {
+
+                    docker.withRegistry(ECRURL, ECRCRED)
+                    {
+                        docker.image(IMAGE).push()
+                    }
+                }
+            }
+         }
+    }
+
+    post
+    {
+        always
+        {
+            // make sure that the Docker image is removed
+            sh "docker rmi $IMAGE | true"
+        }
+    }
+
+}
+}
